5d6b26798c06a2d52a31bb2e49d9675ed834d039
Tjerk Woudsma <tjerkwoudsma@gmail.com>
2021-03-18 16:44:14 +0100

    add test watch

--- test-latest.js
+++ test-latest.js
@@ -1,21 +1,25 @@
-const { join } = require('path')
-const { execSync } = require('child_process');
-const { readdirSync, statSync } = require('fs')
-const { last } = require('lodash');
+const { join } = require("path");
+const { execSync } = require("child_process");
+const { readdirSync, statSync } = require("fs");
+const { last } = require("lodash");
 
-const dirs = p => readdirSync(p)
-  .filter(f => statSync(join(p, f)).isDirectory());
+const dirs = (p) =>
+  readdirSync(p).filter((f) => statSync(join(p, f)).isDirectory());
 
-const YEAR = '2021';
+const YEAR = "2021";
 
 const challenges = dirs(YEAR);
 const latest = last(challenges.sort());
-const testWeek = process.argv[2] || latest;
+const testWeek = process.argv.filter((e) => !e.includes("--"))[2] || latest;
 
 console.log(`TESTING CHALLENGE: ${YEAR}/${testWeek}\n\n`);
 
 try {
-  execSync(`jest --runInBand --rootDir=${YEAR}/${testWeek}`, { stdio: 'inherit' });
-} catch (e) {
+  const args = ["--runInBand", `--rootDir=${YEAR}/${testWeek}`];
 
+  if (process.argv.includes("--watch")) args.push("--watch");
+
+  execSync(`jest ${args.join(" ")}`, { stdio: "inherit" });
+} catch (e) {
+  console.error(e);
 }
