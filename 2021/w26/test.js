1c21bbc010e13777f00548fc6596799ad5a3c2fd
Lee Freeman <lee@passionatepeople.io>
2021-07-01 17:10:47 +0200

    use validator function in test script

--- 2021/w26/test.js
+++ 2021/w26/test.js
@@ -1,22 +1,23 @@
 const fs = require("fs");
 const path = require("path");
 const spec = require("./test-cases/spec.json");
+const validator = require("./validator");
 const solutions = fs.readdirSync(path.resolve(__dirname, "solutions"));
 
 describe.each(solutions)("%s", (filename) => {
   const evaluate = require(path.resolve(__dirname, "solutions", filename));
   test.each([
-    ['ab', ["ab", "ba"]],
-    ['abc', ["abc", "acb", "bac", "bca", "cab", "cba"]],
-    ['aac', ["aac", "aca", "caa"]],
+    ["ab", ["ab", "ba"]],
+    ["abc", ["abc", "acb", "bac", "bca", "cab", "cba"]],
+    ["aac", ["aac", "aca", "caa"]],
   ])("%j = %j", (input, result) => {
-    expect(evaluate(input)).toEqual(expect.arrayContaining(result));
+    expect(validator(evaluate(input), result)).toBe(true);
   });
 
   test.each(spec.map(({ inputs, result }) => [inputs[0], result]))(
     "%j = %j",
     (input, result) => {
-      expect(evaluate(input)).toEqual(expect.arrayContaining(result));
+      expect(validator(evaluate(input), result)).toBe(true);
     }
   );
 });
